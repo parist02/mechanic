14ed349a71f9e1ee6f1ed6b257f58d449ef2d1f0
Panayiotis Aristodemou <57769091+Yiotis98@users.noreply.github.com>
2020-02-09 22:27:44 +0200

    creating tabs

--- src/main/java/com/mechanic/code/MainScreen.java
+++ src/main/java/com/mechanic/code/MainScreen.java
@@ -1,13 +1,17 @@
 package com.mechanic.code;
+
 import javafx.application.Application;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.Tab;
+import javafx.scene.control.TabPane;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
 import javafx.scene.text.Font;
 import javafx.scene.text.FontPosture;
 import javafx.scene.text.FontWeight;
@@ -20,70 +24,42 @@ public class MainScreen extends Application{
     public static Font font=Font.font("Arial",FontWeight.NORMAL, FontPosture.REGULAR,18);
 
     @Override
+    public void init() throws Exception {
+        super.init();
+        System.out.println("The application is starting");
+    }
+
+    @Override
     public void start(final Stage stage) {
-        //Labels
-        final Label labelName=new Label("Name:");
-        labelName.setPadding(padding);
-        labelName.setFont(font);
-        final Label labelSurname=new Label("Surname:");
-        labelSurname.setPadding(padding);
-        labelSurname.setFont(font);
-        final Label labelCarNumbers=new Label("Car Numbers:");
-        labelCarNumbers.setFont(font);
-        labelCarNumbers.setPadding(padding);
-        final Label labelTitle=new Label("Form");
-        labelTitle.setPadding(padding);
-        labelTitle.setFont(Font.font("Arial",FontWeight.BOLD,FontPosture.REGULAR,20));
-
-
-        //TextFields
-        final TextField textFieldName=new TextField();
-        textFieldName.setPadding(padding);
-        textFieldName.setFont(font);
-        final TextField textFieldSurname=new TextField();
-        textFieldSurname.setPadding(padding);
-        textFieldSurname.setFont(font);
-        final TextField textFieldCarNumbers=new TextField();
-        textFieldCarNumbers.setFont(font);
-        textFieldCarNumbers.setPadding(padding);
-
-        //Button
-        final Button buttonPrint =new Button("Print");
-        buttonPrint.setPadding(padding);
-        final Button buttonCancel=new Button("Cancel");
-        buttonCancel.setPadding(padding);
-        buttonCancel.setOnAction(e->{
-            stage.close();
-            exit(0);
-        });
-        buttonPrint.setOnAction(e->{
-            final PrintPreview printPreview=new PrintPreview(textFieldName.getText(),textFieldSurname.getText(),textFieldCarNumbers.getText());
-            printPreview.stagePrint.show();
+        TabPane tabPane = new TabPane();
+        Form form=new Form();
+    
+        
+        Button buttonForm=new Button("Show form");
+        buttonForm.setPadding(padding);
+        buttonForm.setFont(font);
+        Label labelInvoice=new Label("Press the button to show the print form");
+        labelInvoice.setPadding(padding);
+        labelInvoice.setFont(font);
+        VBox boxInvoice=new VBox(buttonForm,labelInvoice);
+        boxInvoice.setPadding(padding);
+        buttonForm.setOnAction(e->{
+            form.showForm();
         });
 
-        //Scene control
-        final GridPane grid=new GridPane();
-        grid.setPadding(padding);
-        grid.setHgap(20);
-        grid.setVgap(20);
-        grid.add(labelTitle,0,0,2,1);
-        grid.add(labelName,0,1);
-        grid.add(textFieldName,1,1);
-        grid.add(labelSurname,0,2);
-        grid.add(textFieldSurname,1,2);
-        grid.add(labelCarNumbers,0,3);
-        grid.add(textFieldCarNumbers,1,3);
-        final HBox box=new HBox(buttonPrint,buttonCancel);
-        box.setSpacing(20);
-        box.setPadding(padding);
-        buttonCancel.setAlignment(Pos.BASELINE_RIGHT);
-        grid.add(box,0,4,2,1);
-        grid.setGridLinesVisible(false);
-        final Scene scene=new Scene(grid);
-        scene.getStylesheets().add("stylemain.css");
+        Tab tab1=new Tab("Customers",new Label("Showing all customers"));
+        Tab tab2=new Tab("Parts",new Label("Showing all parts"));
+        Tab tab3=new Tab("Invoices", boxInvoice);
+
+        tabPane.getTabs().add(tab1);
+        tabPane.getTabs().add(tab2);
+        tabPane.getTabs().add(tab3);
+
+        Scene scene=new Scene(tabPane);
         stage.setScene(scene);
-        stage.setTitle("Form");
+        stage.setTitle("MainScreen");
         stage.show();
+
     }
 
 
